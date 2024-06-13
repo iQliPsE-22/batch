d0444afc61ea1593ad24d6b6af5f3e9dd8479f79
Deepak Kumar <deepaklg02@gmail.com>
2024-06-13 22:58:01 +0530

    Add

--- app/component/Button.tsx
+++ app/component/Button.tsx
@@ -1,4 +1,10 @@
-const Button = (props) => {
+import React from 'react';
+
+type ButtonProps = {
+  children: React.ReactNode;
+};
+
+const Button = (props: ButtonProps) => {
   return (
     <div>
       <button className="px-4 py-2 text-white bg-black rounded-sm w-48 h-14 flex items-center justify-center">
